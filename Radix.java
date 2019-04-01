78598d9bb806410f14c94927839c8b474460ced4
Julian Wong <jwong01@stuy.edu>
2019-04-01 14:36:39 -0400

    finds largest number and number of digits in it

--- Radix.java
+++ Radix.java
@@ -1,8 +1,19 @@
 public class Radix {
   public static void radixsort(int[] dat) {
-
+    int h = -2147483648;
+    for(int q = 0; q < dat.length; q++) {
+      if(dat[q] > h) {h = dat[q];}
+    }
+    int ll = String.valueOf(h).length();
+    System.out.println(ll);
   }
   public static void main(String[] sdjf) {
-    
+    int[] test = {170, 45, 75, 90, 802, 24, 2, 66};
+    radixsort(test);
+    System.out.print("[");
+    for(int q = 0; q < test.length; q++) {
+      System.out.print(test[q] + ", ");
+    }
+    System.out.print("]");
   }
 }
