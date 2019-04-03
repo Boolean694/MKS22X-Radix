d2c479f64331af6e9ab8b665ec93c608c17776ee
Julian Wong <jwong01@stuy.edu>
2019-04-03 00:51:26 -0400

    messed up what the hell

--- Radix.java
+++ Radix.java
@@ -1,3 +1,4 @@
+import java.lang.Math;
 public class Radix {
   public static void radixsort(int[] dat) {
     int h = -2147483648;
@@ -5,7 +6,24 @@ public class Radix {
       if(dat[q] > h) {h = dat[q];}
     }
     int ll = String.valueOf(h).length();
-    System.out.println(ll);
+    //MyLinkedList[] buckets = new MyLinkedList[ll];
+    for(int w = 0; w < ll; w++) {
+
+    }
+  }
+  private static void sortdig(int[] dat, int dig) {
+    for(int q = 0; q < dat.length; q++) {
+      int sm = 9;
+      int smind = 0;
+      for(int w = q; w < dat.length; w++) {
+        if((dat[q] / ((int)Math.pow(10, dig))) % 10 < sm) {sm = (dat[q] / ((int)Math.pow(10, dig))) % 10; smind = w;}
+      }
+      int mtf = dat[smind];
+      for(int w = smind - 1; w >= q; w--) {
+        dat[w] = dat[w - 1];
+      }
+      dat[q] = dat[smind];
+    }
   }
   public static void main(String[] sdjf) {
     int[] test = {170, 45, 75, 90, 802, 24, 2, 66};
